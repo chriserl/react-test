a66e7681a1b68441625b1e4d7d9793caeb62fb86
chriserl <chriserldev@gmail.com>
2020-08-11 16:48:00 +0000

    build commit

---
+++ build/precache-manifest.8716898f7ca5ea0a2523855def0af11e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878896bb706ea11d45f9112aeaff25d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "94dc441490500e4d5632",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1e933889b1a9cb0af47",
+    "url": "./static/js/2.d3b923cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d3b923cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94dc441490500e4d5632",
+    "url": "./static/js/main.b186d08b.chunk.js"
+  },
+  {
+    "revision": "9b74c69f8b5d80a04967",
+    "url": "./static/js/runtime-main.3d9406cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);

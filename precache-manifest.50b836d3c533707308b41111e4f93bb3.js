15eedc82da689a8502e3fb82e38c0f74c081caf9
chriserl <chriserldev@gmail.com>
2020-08-11 16:50:28 +0000

    new build

---
+++ precache-manifest.50b836d3c533707308b41111e4f93bb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1547b18fda6931a19757b623eec4b801",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "24937eb6b1ad1d3f8b23",
+    "url": "/react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1e933889b1a9cb0af47",
+    "url": "/react-test/static/js/2.d3b923cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.d3b923cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24937eb6b1ad1d3f8b23",
+    "url": "/react-test/static/js/main.7b110c71.chunk.js"
+  },
+  {
+    "revision": "40958326a7a5e8d40d0e",
+    "url": "/react-test/static/js/runtime-main.79a7e98c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8716898f7ca5ea0a2523855def0af11e.js"
+  "/react-test/precache-manifest.50b836d3c533707308b41111e4f93bb3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
